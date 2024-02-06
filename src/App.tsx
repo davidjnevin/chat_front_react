9144b14376d6617b19a4f977360ac7bc56684fcd
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-02-06 11:42:26 +0100

    import GetAllIds and GetCleaningById components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import './App.css';
 import { Heading } from './components/Heading';
 import { Description } from './components/Description';
-import ChatInputForm from './components/ChatInputForm';
+import { ChatInputForm } from './components/ChatInputForm';
+import GetAllIds from './components/GetAllIds';
+import GetCleaningById from './components/GetCleaningById';
 
 function App() {
+
+
 	return (
 		<main>
 			<article className="l-design-width">
+			<GetCleaningById />
 				<Heading />
 				<Description />
 				<ChatInputForm />
