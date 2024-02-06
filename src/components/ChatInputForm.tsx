27ba332198cdb0176da5ec53852ab5e77de45c05
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2024-02-06 23:38:24 +0100

    implement initial post with correct data returned endpoint

--- src/components/ChatInputForm.tsx
+++ src/components/ChatInputForm.tsx
@@ -1,7 +1,10 @@
 import * as yup from 'yup';
 import { Formik, Form, Field, } from 'formik';
+import { useState } from 'react';
+import axios, { AxiosResponse } from 'axios';
 
 
+const url = 'http://127.0.0.1:8000/clean/cleanings/'
 const maxChatLength = 1000;
 
 const ChatSchema = yup.object({
@@ -17,6 +20,38 @@ interface ChatInputValues extends yup.InferType<typeof ChatSchema> {
 
 export const ChatInputForm: React.FC<{}> = () => {
 	const initialValues: ChatInputValues = { chatInputValue: '' };
+	const [cleanChatIn, setCleanChatIn] = useState<string>('');
+	const [cleanChatOut, setCleanChatOut] = useState<string>('');
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState<any>(null);
+
+	const fetchCleanChat = async (cleanChatIn: string) => {
+		console.log('cleanChatIn', cleanChatIn);
+		setIsLoading(true);
+		setError(null);
+
+		try {
+			const response: AxiosResponse = await axios.post(
+				url,
+				{
+					"chat_text": cleanChatIn
+				},
+				{
+					headers: {
+						'Content-Type': 'application/json',
+					},
+				}
+			);
+			(response.data.result === "Not found")
+				? setCleanChatOut('No chat found\n')
+				: console.log(response);
+			// : setCleanChatOut(response.data.result.cleaned_chat);
+		} catch (error) {
+			setError(error);
+		} finally {
+			setIsLoading(false);
+		}
+	};
 	return (
 		<div className="card">
 			<Formik
@@ -24,7 +59,8 @@ export const ChatInputForm: React.FC<{}> = () => {
 				validationSchema={ChatSchema}
 				onSubmit={(values, actions) => {
 					console.log({ values, actions });
-					alert(JSON.stringify(values, null, 2));
+					setCleanChatIn(values.chatInputValue);
+					fetchCleanChat(cleanChatIn);
 				}}
 				onReset={(values, actions) => {
 					console.log({ values, actions });
